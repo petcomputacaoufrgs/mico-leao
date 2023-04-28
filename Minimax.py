df19e5f3b24b243b4b8465b136d26e5f1f1485d3
Galeano Folle Domingues <88506164+TakatJJ@users.noreply.github.com>
2023-04-28 11:37:27 -0300

    Tic Tac Toe
    Uses Minimax to map optimal plays.

---
+++ Minimax.py
@@ -0,0 +1,64 @@
+# Description: This program is a Tic Tac Toe game that uses the Minimax algorithm to play against the user.
+import PySimpleGUI as sg
+from Stuff import *
+sg.theme('DarkAmber') 
+width = 15
+layout = [  [sg.Text('Welcome to Tic Tac Toe. '), sg.Text("Press in your play: ")],
+            [sg.Button(" ",size=(width,width),key="0"), sg.Button(" ",size=(width,width),key="1"), sg.Button(" ",size=(width,width),key="2")],
+            [sg.Button(" ",size=(width,width),key="3"), sg.Button(" ",size=(width,width),key="4"), sg.Button(" ",size=(width,width),key="5")],
+            [sg.Button(" ",size=(width,width),key="6"), sg.Button(" ",size=(width,width),key="7"), sg.Button(" ",size=(width,width),key="8")]]
+
+def main():
+    window = sg.Window("Tic Tac Toe", layout)
+    actualBoard = Game()
+    while True:
+        
+        event,values = window.read()
+        # print(window.AllKeysDict)
+        if sg.WINDOW_CLOSED == event:
+            exit()
+        # if event == "0":
+        #     pass
+            # window[int(event)].update("X")
+        
+        
+        # teste(actualBoard) #Tests if the evaluation process is working properly
+        
+        # while not Terminal(actualBoard.gameBoard):
+        # actualBoard.show()
+        play = int(event[0])
+        if actualBoard.IsValid(int(play)):
+            actualBoard.Assing(int(play))
+            # actualBoard.show()
+            board = actualBoard.gameBoard[:]
+            InitNode = NodeGame(board)
+            if actualBoard.Evaluate(actualBoard.Value()) == 0:
+                Minimax(InitNode, True)
+                best = 0
+                index = 0
+                counter = 0
+                for children in InitNode.children:
+                    
+                    if children.getValue() >= best:
+                        best = children.getValue()
+                        counter = index
+                    index +=1
+                        
+                actualBoard.gameBoard = InitNode.children[counter].gameBoard
+                caller = 0
+                for cells in actualBoard.gameBoard:
+                    window[str(caller)].update(cells)
+                    caller+=1
+                    # if actualBoard.Evaluate(actualBoard.Value()) != 0:
+                    #     if AfterGame(actualBoard, window) == False:
+                    #         exit()
+            else:
+                if AfterGame(actualBoard, window) == False:
+                    exit()
+        else:
+            sg.popup("Invalid Play")
+            # print("///////////////////////////////////////")
+        # actualBoard.show()
+  
+if __name__ == "__main__":
+    main()
