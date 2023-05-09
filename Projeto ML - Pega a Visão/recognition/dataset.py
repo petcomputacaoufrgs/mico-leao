30bec7fd1dadd2c69e006f6609836523bdbe03b9
JHLM <josehenriquelm@gmail.com>
2023-05-09 14:52:33 -0300

    Dataset now uses torch image reading functions

--- Projeto ML - Pega a Visão/recognition/dataset.py
+++ Projeto ML - Pega a Visão/recognition/dataset.py
@@ -1,8 +1,11 @@
 from torch.utils.data import Dataset
+from torchvision.io import read_image
 import torch
+import torchvision
 import pandas as pd
 import os
 import cv2 as cv
+import numpy as np
 from itertools import combinations
 
 class FacesDataset(Dataset):
@@ -23,25 +26,34 @@ class FacesDataset(Dataset):
 
         subject1 = img_pair[0][0]
         subject2 = img_pair[1][0]
-        same_method = (subject1 == subject2)
+        label = 1 if (subject1 == subject2) else 0
         img_path1 = os.path.join(self.root_dir, img_pair[0][1])
         img_path2 = os.path.join(self.root_dir, img_pair[1][1])
 
-        img1 = cv.imread(img_path1)
-        img2 = cv.imread(img_path2)
+        img1 = read_image(img_path1)
+        img2 = read_image(img_path2)
         
         if self.transform:
             img1 = self.transform(img1)
             img2 = self.transform(img2)
 
-        sample = (img1, img2, same_method)
+        sample = (img1, img2, label)
         return sample
 
     def getImageFromSample(self, sample):
-        shape1, shape2 = sample[0].shape[:-1], sample[1].shape[:-1]
+
+        im1 = np.moveaxis(sample[0].numpy(), [0, 1, 2], [2, 0, 1])
+        im2 = np.moveaxis(sample[1].numpy(), [0, 1, 2], [2, 0, 1])
+        
+        shape1, shape2 = im1.shape[:-1], im2.shape[:-1]
         new_shape = shape1 if shape1 > shape2 else shape2
-        im1 = cv.resize(sample[0], new_shape)
-        im2 = cv.resize(sample[1], new_shape)
+        
+        im1 = cv.resize(im1, new_shape)
+        im2 = cv.resize(im2, new_shape)
+
+        im1 = cv.cvtColor(im1, cv.COLOR_RGB2BGR)
+        im2 = cv.cvtColor(im2, cv.COLOR_RGB2BGR)
+
         image = cv.hconcat([im1, im2])
         text = 'SAME' if sample[2] else 'DIFFERENT'
         color = (0,255,0) if sample[2] else (0,0,255)
@@ -51,7 +63,7 @@ class FacesDataset(Dataset):
 
 
 if __name__ == "__main__":
-    dataset = FacesDataset('facedataset.csv', 'faces', None)
+    dataset = FacesDataset('facedataset.csv', 'faces')
     print(f'Displaying {len(dataset)} samples')
     for sample in dataset:
         image = dataset.getImageFromSample(sample)
