838abc4f4a08d5340d0af30a6bee344ce520c7f2
jvhendrix <joaovitorsouza5000@gmail.com>
2023-06-03 00:28:02 -0300

    added dark mode

--- Minimax/main.py
+++ Minimax/main.py
@@ -1,10 +1,13 @@
 import sys
 import gui
+import qdarkstyle
 from PySide6 import QtWidgets
 
 
 if __name__ == "__main__":
    app = QtWidgets.QApplication([])
+   dark_stylesheet = qdarkstyle.load_stylesheet_pyqt5()
+   app.setStyleSheet(dark_stylesheet)
 
    game = gui.MainWindow()
    game.launch()
